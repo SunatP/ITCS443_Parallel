bd5803508e7c1a65ce2ee0d1198439f414187a56
Sunat.P <31818562+SunatP@users.noreply.github.com>
2019-12-08 20:53:01 +0700

    update EX7

---
+++ Exercise/EX7/README.md
@@ -0,0 +1,67 @@
+# Exercise 7 Advanced Topics in CUDA 
+
+1.  From the following sequential Rank sort algorithm that allows duplicate numbers, implement a parallel version in CUDA. เราจะต้องอิงอัลกอริทึ่มตาม Ranksort โดยที่สามารถ sort เลขที่ซ้ำกันได้
+
+```C++
+for (i = 0; i < n; i++) /* for each number */ 
+{ 
+    x = 0; for (j = 0; j < n; j++) /* count number less than it */ 
+    if ((a[i] > a[j]) || (a[i] == a[j]) && (j <i))
+    x++; 
+    b[x] = a[i];  /* copy number into correct place */ 
+}
+```
+
+เราจะ implement ตัว rank sort นี้เข้ามาใช้ในโค้ดของเรา
+
+```C++
+#include <stdio.h>
+#include <stdlib.h>
+#include <thrust/sort.h> // ตรงนี้เราจะใช้ Header ของ Nvidia เข้ามาช่วย sort
+#define array_size 64 // ขนาดอาเรย์
+#define thread 16 // define ไว้สำหรับ thread
+__global__ void rank_sort(int *data, int *result)
+{
+    int i,j,position;
+    i = blockIdx.x * blockDim.x + threadIdx.x;
+    int self = data[i];
+    for(;i < array_size; i++) // เนื่องจากเราassign i ไว้แล้วเลยไม่ต้อง assign ค่าให้มันอีกรอบ
+    {
+        position = 0;
+    for(j = 0; j < array_size; j++)
+        if(( self > data[j]) || (self == data[j]) && (j < i))
+            position+=1;
+    result[position] = self;
+    }
+} 
+
+int main(int argc, char *argv[]){
+  int *arr = (int *) malloc(sizeof(int)*array_size);
+  int i;
+  int *data = (int *) malloc(sizeof(int)*array_size);
+  int *result = (int *) malloc(sizeof(int)*array_size);
+  int size =  sizeof(int)*thread;
+  srand(123);
+  for(i = 0; i < array_size; i++)
+  {
+     arr[i] = rand() % 50;
+  }
+  cudaMalloc( (void**) &data, thread*array_size);
+  cudaMalloc( (void**) &result, thread*array_size);
+  thrust::sort(arr, arr + size); // ตรงนี้คือ function sort ของ Nvidia สามารถใช้ sort ได้แบบปกติเลย
+  cudaMemcpy(data,arr,size, cudaMemcpyHostToDevice);
+  dim3 dimBlock(thread);
+  dim3 dimGrid(array_size/thread - 1);
+  rank_sort<<<dimGrid,dimBlock>>>(data,result);
+  cudaMemcpy(arr,data,size,cudaMemcpyDeviceToHost);
+  printf(" Sorted Data \n");
+  for(i = 0 ; i < array_size ; i++)
+  {
+    printf("%d ",arr[i]);
+  }
+  printf("\n");
+  cudaFree(data);  
+  cudaFree(result);
+  return 0;
+}
+```
--- Exercise/EX7/cuda1.cu
+++ Exercise/EX7/cuda1.cu
@@ -1,55 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define thread 256
-#define arraySize 64
-/**
- From the following sequential Rank sort algorithm that allows duplicate numbers, 
- implement a parallel version in CUDA. 
-**/
-
-__global__ void RankSort(int *data, int *result)
+#include <thrust/sort.h>
+#define array_size 64
+#define thread 16
+__global__ void rank_sort(int *data, int *result)
 {
-   int i,j,position;
-   
-   i = threadIdx.x;
-   int self = data[i];
-
-   for(j = 0 ; j < arraySize ; j++)
-   {
-       position = 0;
-       if((self > data[i]) || (self == data[j]) && (j < i))
-       {
-           position++;
-       }
-       result[position] = self;
-   }
-}
-int main(int argc,char *argv[])
-{
-    int arr[arraySize];
-    int i;
-    int size = thread*sizeof(int);
-    int *data, *result;
-    srand(time(0));
-    for(i = 0 ; i < arraySize ; i++)
+    int i,j,position;
+    i = blockIdx.x * blockDim.x + threadIdx.x;
+    int self = data[i];
+    for(;i < array_size; i++)
     {
-         arr[i] = rand() % 50;
+        position = 0;
+    for(j = 0; j < array_size; j++)
+        if(( self > data[j]) || (self == data[j]) && (j < i))
+            position+=1;
+    result[position] = self;
     }
-    cudaMalloc( (void**)&data,size);
-    cudaMalloc( (void**)&result,size);
-	cudaMemcpy( data, arr, size, cudaMemcpyHostToDevice);
+}   
 
-    RankSort<<<thread/arraySize,thread>>>(data,result);
+int main(int argc, char *argv[]){
+  int *arr = (int *) malloc(sizeof(int)*array_size);
+  int i;
+  int *data = (int *) malloc(sizeof(int)*array_size);
+  int *result = (int *) malloc(sizeof(int)*array_size);
+  int size =  sizeof(int)*thread;
+//   (float *)malloc(blocks*threads_per_block*sizeof(float));
+  srand(123);
+  for(i = 0; i < array_size; i++)
+  {
+     arr[i] = rand() % 50;
+  }
+  printf(" Working Ok\n");
+  cudaMalloc( (void**) &data, thread*array_size);
+  cudaMalloc( (void**) &result, thread*array_size);
+  printf(" Mallock Ok\n");
+  thrust::sort(arr, arr + size);
+  cudaMemcpy(data,arr,size, cudaMemcpyHostToDevice);
+  printf(" Copy Ok\n");
+  dim3 dimBlock(thread);
+  dim3 dimGrid(array_size/thread - 1);
+  rank_sort<<<dimGrid,dimBlock>>>(data,result);
+  printf(" Function Ok\n");
+  cudaMemcpy(arr,data,size,cudaMemcpyDeviceToHost);
+  printf(" Copy Back Ok\n");
 
-    cudaMemcpy( arr, result,size,cudaMemcpyDeviceToHost);
-    cudaFree(data);
-    cudaFree(result);
-
-    printf("\n sorted data");
-    for(i = 0 ; i< arraySize ;i++)
-    {
-        printf("%d", data[i]);
-    }
-    printf("\n");
-    return 0;
+  printf(" Sorted Data \n");
+  for(i = 0 ; i < array_size ; i++)
+  {
+    printf("%d ",arr[i]);
+  }
+  printf("\n");
+  printf(" Sorted OK \n");
+  cudaFree(data);  
+  cudaFree(result);
+  return 0;
 }
